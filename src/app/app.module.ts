6d458cfa8d524888ae95c1fd7ff27f0ca476f494
UMUNNAKWE, JOHNLEONARD CHINAEMEREM <johnleonarduc@gmail.com>
2021-01-11 04:27:41 +0100

    fix footer mobile and faq section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faFacebook, faTwitter, faInstagram, faYoutube, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope, faPhone, faMapMarker} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-footer',
@@ -11,5 +13,15 @@ export class FooterComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  fafacebook = faFacebook;
+  faTwitter = faTwitter;
+  faInstagram = faInstagram;
+  faYoutube = faYoutube;
+  faLinkedin = faLinkedin;
+  faPhone = faPhone;
+  faEnvelope = faEnvelope;
+  faWhatsapp = faWhatsapp;
+  faMapMarker = faMapMarker;
 
 }
